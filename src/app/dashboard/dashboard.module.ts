9c16077e91958e6b3edb6358de95e755a4c139c7
Josue Baez <josue.baez@coderhouse.com>
2023-07-06 19:52:52 -0300

    ADD, se agrego el dashboard module

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
+  exports: [
+    DashboardComponent
+  ]
+})
+export class DashboardModule { }
